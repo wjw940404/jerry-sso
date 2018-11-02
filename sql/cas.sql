adab6e7488049c81e115f6a1540c682bb387650d
wujingwei <Jerry.wjw@xinwis.com>
2018-11-02 16:38:35 +0800

    update cas client

---
+++ sql/cas.sql
@@ -0,0 +1,27 @@
+-- ----------------------------
+-- Create Database cas
+-- ----------------------------
+CREATE DATABASE cas
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  `age` int(3) NULL DEFAULT NULL COMMENT '年龄',
+  `sex` int(1) NULL DEFAULT NULL COMMENT '性别',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 24, 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
